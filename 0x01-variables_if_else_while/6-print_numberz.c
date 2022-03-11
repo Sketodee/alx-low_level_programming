3e915d861ba62e230195a1d707fa80be2350f247
root <shobandejames@gmail.com>
2022-03-11 13:03:03 -0800

    first

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+
 /**
-*main - entry point, print the alphabets
-*with only two putchar()s
-*
+* main - prints all single digit numbers of base
+* 10 starting from 0, followed by a new line
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	int i = 0;
-	while (i < 10)
-	{
-		printf("%d", i);
-		i++;
-	}
-	printf("\n");
-	return (0);
+int n;
+
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
 }
